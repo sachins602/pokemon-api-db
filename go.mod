a50197386bf063bf1f7e29fad37729831cd8816c
sachins602 <sachinsapkota4@gmail.com>
2022-07-27 02:17:47 +0545

    Save Pokemon data to Database

--- go.mod
+++ go.mod
@@ -3,6 +3,15 @@ module github.com/sachins602/pokemon-api-db
 go 1.17
 
 require (
+	github.com/joho/godotenv v1.4.0
+	gorm.io/driver/mysql v1.3.5
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mtslzr/pokeapi-go v1.4.0 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	gorm.io/gorm v1.23.8 // indirect
 )
--- main.go
+++ main.go
@@ -2,26 +2,65 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
 	"github.com/mtslzr/pokeapi-go"
-	"net/http"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
-	pokemonData, err := pokeapi.Pokemon("1")
-	if err != nil {
-		fmt.Println(err)
-	}
-	name := pokemonData.Name
-	attack := pokemonData.Stats[4].BaseStat
-	spriteUrl := fmt.Sprintf("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/%d.png", 1)
 
-	sprite, err := http.Get(spriteUrl)
-
-	if err != nil {
-		fmt.Println(err)
+	// Load environment variables from file.
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("failed to load environment variables: %v", err)
 	}
 
-	fmt.Println(sprite)
+	// Connect to PlanetScale database using DSN environment variable.
+	db, err := gorm.Open(mysql.Open(os.Getenv("DSN")), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
+	if err != nil {
+		log.Fatalf("failed to connect to PlanetScale: %v", err)
+	}
 
-	fmt.Println(name, attack, spriteUrl)
+	for index := 1; index < 500; index++ {
+		i := fmt.Sprintf("%d", index)
+		pokemonData, err := pokeapi.Pokemon(i)
+		if err != nil {
+			fmt.Println(err)
+		}
+		id := index
+		name := pokemonData.Name
+		attack := pokemonData.Stats[4].BaseStat
+		spriteUrl := fmt.Sprintf("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/%d.png", index)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		pokemon := &PokemonStruct{
+			Id:        id,
+			Name:      name,
+			Attack:    attack,
+			SpriteUrl: spriteUrl,
+		}
+
+		db.Table("Pokemon").Create(&pokemon)
+		fmt.Println("Pokemon added to database")
+
+		fmt.Println(name, attack, spriteUrl)
+		time.Sleep(time.Millisecond * 100)
+	}
+
+}
+
+type PokemonStruct struct {
+	Id        int    `gorm:"primary_key column:id"`
+	Name      string `gorm:"column:name"`
+	Attack    int    `gorm:"column:attack"`
+	SpriteUrl string `gorm:"column:spriteUrl"`
 }
