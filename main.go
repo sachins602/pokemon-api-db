76d201ac1b6b806edb13556f711537fc5434746e
sachins602 <sachinsapkota4@gmail.com>
2022-07-26 23:32:35 +0545

    get needed ifo

--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/mtslzr/pokeapi-go"
+	"net/http"
 )
 
 func main() {
-	a, err := pokeapi.Pokemon("1")
+	pokemonData, err := pokeapi.Pokemon("1")
+	if err != nil {
+		fmt.Println(err)
+	}
+	name := pokemonData.Name
+	attack := pokemonData.Stats[4].BaseStat
+	spriteUrl := fmt.Sprintf("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/%d.png", 1)
+
+	sprite, err := http.Get(spriteUrl)
+
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(a, err)
+	fmt.Println(sprite)
+
+	fmt.Println(name, attack, spriteUrl)
 }
